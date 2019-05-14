517e7274a1e6a82e3f8ed959e0dc18adae8dafaf
rmcbride2121 <rmcbride2121@gmail.com>
2019-05-13 19:36:19 -0500

    new project

---
+++ fetch.js
@@ -0,0 +1,26 @@
+let arrayOfPosts;
+
+window.onload = function() {
+  getPosts()
+
+}
+
+const getPosts = () => {
+  fetch('http://jsonplaceholder.typicode.com/posts')
+    .then(res => res.json())
+    .then(posts => arrayOfPosts = posts)
+}
+
+const consolePosts = () => {
+  console.log(arrayOfPosts)
+}
+
+const displayPost = () => {
+  const allPosts = document.getElementById('all-posts')
+  arrayOfPosts.map((post, index) => {
+    const li = document.createElement('li')
+    const text = document.createTextNode(`#${index}, Title: ${post.title}:  ${post.body}, by user: ${post.userId}`)
+    li.appendChild(text)
+    allPosts.append(li)
+  })
+}
